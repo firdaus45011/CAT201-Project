11fe48b3d2ff694cc8a7f9ef5349253f11a6efde
firdaus45011 <firdaus45011@gmail.com>
2023-01-25 22:24:54 +0800

    Version 1.0

---
+++ app/src/main/java/com/android/iunoob/bloodbank/fragments/BloodConnection.java
@@ -0,0 +1,30 @@
+package com.android.iunoob.bloodbank.fragments;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.iunoob.bloodbank.R;
+
+public class BloodConnection extends Fragment {
+    private TextView textView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bloodconnection, container, false);
+        getActivity().setTitle("Blood Connection");
+
+        textView = view.findViewById(R.id.connection);
+        Linkify.addLinks(textView, Linkify.ALL);
+        return  view;
+    }
+}
