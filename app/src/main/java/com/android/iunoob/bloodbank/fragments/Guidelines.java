ef2c970c428191854713b9b3e5adfd1343dbb5d0
NazmiHashim <nazmih79@gmail.com>
2023-01-25 22:49:08 +0800

    update

---
+++ app/src/main/java/com/android/iunoob/bloodbank/fragments/Guidelines.java
@@ -0,0 +1,34 @@
+package com.android.iunoob.bloodbank.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.iunoob.bloodbank.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Guidelines#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Guidelines extends Fragment {
+
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_guidelines, container, false);
+        getActivity().setTitle("Guidelines");
+
+        textView = view.findViewById(R.id.guideline_text);
+        Linkify.addLinks(textView, Linkify.ALL);
+        return  view;
+    }
+}
---
+++ app/src/main/java/com/android/iunoob/bloodbank/viewmodels/DonorData.java
@@ -0,0 +1,83 @@
+package com.android.iunoob.bloodbank.viewmodels;
+
+/***
+ Project Name: BloodBank
+ Project Date: 10/17/18
+ Created by: imshakil
+ Email: mhshakil_ice_iu@yahoo.com
+ ***/
+
+public class DonorData {
+
+    private int TotalDonate;
+    private String LastDonate, Name, Contact, UID, Address,NextDonate;
+
+
+    public DonorData() {
+
+    }
+
+    public DonorData(int totalDonate, String lastDonate,String nextDonate, String Name, String Contact, String Address, String UID) {
+        this.TotalDonate = totalDonate;
+        this.LastDonate = lastDonate;
+        this.NextDonate = nextDonate;
+        this.Name = Name;
+        this.Contact = Contact;
+        this.UID = UID;
+        this.Address = Address;
+    }
+
+    public int getTotalDonate() {
+        return TotalDonate;
+    }
+
+    public void setTotalDonate(int totalDonate) {
+        this.TotalDonate = totalDonate;
+    }
+
+    public String getLastDonate() {
+        return LastDonate;
+    }
+    public String getNextDonate() {
+        return NextDonate;
+    }
+    public void setNextDonateDonate(String NextDonate) {
+        this.NextDonate= NextDonate;
+    }
+
+    public void setLastDonate(String lastDonate) {
+        this.LastDonate = lastDonate;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setDonorName(String donorName) {
+        this.Name = Name;
+    }
+
+    public String getContact() {
+        return Contact;
+    }
+
+    public void setContact(String donorContact) {
+        this.Contact = Contact;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
